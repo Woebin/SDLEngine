7556eed004fc6efcfe88e69f195deda2de4deccf
Theo Walther <woebin@gmail.com>
2017-01-09 12:14:26 +0100

    It compiles! It does nothing, but it compiles.

--- Engine/Source.cpp
+++ Engine/Source.cpp
@@ -2,7 +2,17 @@
 #include "GameEngine.h"
 
 
-
+int main(int argc, char** argv) {
+	//Frame frame;
+	/*Label* lab = Label::getInstance({ 250,100,100,100 }, "0");
+	frame.add(lab);
+	Component* c1 = new MyButton({ 100,100,100,100 }, "Increase", lab);
+	frame.add(c1);
+	Component* c2 = new MyButton({ 400,100,100,100 }, "Decrease", lab);
+	frame.add(c2);*/
+	//frame.run();
+	return 0;
+}
 
 Source::Source()
 {
